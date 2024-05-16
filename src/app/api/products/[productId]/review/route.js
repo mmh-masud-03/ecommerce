c40029a9faab73332bfa2752fa1dcd27b4283771
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-05-16 15:50:52 +0600

    add review of product

--- src/app/api/products/[productId]/review/route.js
+++ src/app/api/products/[productId]/review/route.js
@@ -1,11 +1,19 @@
 import connectDB from "@/mongodb";
 import Review from "@/models/Review";
+
 export const POST = async (req, res) => {
   try {
     await connectDB();
-    const allReviews = await Review.find();
-    return new Response(JSON.stringify(allReviews), { status: 200 });
+    const { rating, comment, userId } = await req.json();
+
+    const newReview = await Review.create({
+      user: userId,
+      rating,
+      comment,
+    });
+
+    return new Response(JSON.stringify(newReview), { status: 200 });
   } catch (err) {
-    return new Response("Error fetching reviews", { status: 500 });
+    return new Response("Error creating review", { status: 500 });
   }
 };
