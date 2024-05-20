201fd7dc46ecb5ffaf6ecb5922d4522a4d7dbc2c
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-05-20 22:37:20 +0600

    ready for initial release

--- middleware.js
+++ middleware.js
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server";
-import { verify } from "jsonwebtoken";
-export default middleware = async (req, res) => {
-  const token = req.cookies.authToken;
-  if (!token) {
-    return NextResponse.redirect("/login");
+
+export function middleware(request) {
+  const token = request.cookies.get("userId");
+
+  const { pathname } = request.nextUrl;
+  console.log(pathname, token);
+  if (
+    pathname === "/user/profile" ||
+    pathname === "/user/cart" ||
+    pathname === "/user/orders" ||
+    (pathname === "/user/wishlist" && !token)
+  ) {
+    return NextResponse.redirect(new URL("/user/login", request.url));
   }
-  try {
-    const decoded = verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    return NextResponse.next();
-  } catch (err) {
-    return NextResponse.redirect("/login");
-  }
-};
-export const config = {
-  matcher: /^\/profile/,
-};
+
+  return NextResponse.next();
+}
--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,23 +1,29 @@
-import { cookies } from 'next/headers';
-import Cookies from 'js-cookie';
+import { cookies } from "next/headers";
+import Cookies from "js-cookie";
 
 export async function GET(req) {
   try {
+    // Define the options for cookie removal
     const cookieOptions = {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 0, // Set the maxAge to 0 to remove the cookie
-      path: '/',
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0), // Expire the cookie immediately
+      path: "/",
     };
 
-    cookies().set('authToken', '', cookieOptions);
+    // Set the authToken cookie to expire immediately
+    cookies().set("authToken", "", cookieOptions);
 
-    Cookies.remove('user');
+    // Remove the user cookie from the client-side using js-cookie
+    Cookies.remove("user");
 
-    return new Response(JSON.stringify({ message: 'Logged out successfully' }), {
-      status: 200,
-    });
+    return new Response(
+      JSON.stringify({ message: "Logged out successfully" }),
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
     return new Response(err.message, { status: 500 });
   }
